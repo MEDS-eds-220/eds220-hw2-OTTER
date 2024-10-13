13b8688e57f57a5f2358370f327d67e924b8283f
annieradams <adams.annie11@gmail.com>
2024-10-13 08:38:51 -0700

    finished tests and created dist folder

---
+++ dist-task3/autograder/tests/q6_c.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'q6_c', 'points': 2, 'suites': [{'cases': [{'code': '>>> date_type is pd.Series\nTrue', 'hidden': True, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ dist-task3/student/tests/q7.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q7',
+    'points': 2,
+    'suites': [   {   'cases': [{'code': ">>> pd.read_csv('data/t3_q7_df.csv', index_col='date').equals(aqi_sb)\nFalse", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
