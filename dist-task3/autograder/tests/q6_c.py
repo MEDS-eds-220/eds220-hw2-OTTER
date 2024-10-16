94723f054846ba1256045b91b77e9567fd22ae3b
annieradams <adams.annie11@gmail.com>
2024-10-16 08:46:18 -0700

    finished workaround tests for strings in task 3

--- dist-task3/autograder/tests/q6_c.py
+++ dist-task3/autograder/tests/q6_c.py
@@ -1,3 +1,9 @@
 OK_FORMAT = True
 
-test = {'name': 'q6_c', 'points': 2, 'suites': [{'cases': [{'code': '>>> date_type is pd.Series\nTrue', 'hidden': True, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'q6_c',
+    'points': 2,
+    'suites': [   {   'cases': [{'code': ">>> with open('data/t3_q6c_df.txt', 'r') as file:\n...     type_txt = file.read()\n>>> type_txt == str(date_type)\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
--- dist-task3/student/tests/q6a.py
+++ dist-task3/student/tests/q6a.py
@@ -1,3 +1,9 @@
 OK_FORMAT = True
 
-test = {'name': 'q6a', 'points': 2, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'q6a',
+    'points': 2,
+    'suites': [   {   'cases': [{'code': ">>> assert pd.read_csv('data/t3_q6a_df.csv', index_col=0).equals(aqi_sb)\n", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
