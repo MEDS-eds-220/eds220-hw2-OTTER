6fe11c48f2a8e23d6c65a29cfaed0d9a68d1e099
annieradams <adams.annie11@gmail.com>
2024-10-16 17:27:01 -0700

    task 2 reay for final updates, autograder running, task 3 autograder not running

--- dist-task2/student/tests/q2b.py
+++ dist-task2/student/tests/q2b.py
@@ -1,3 +1,9 @@
 OK_FORMAT = True
 
-test = {'name': 'q2b', 'points': 2, 'suites': [{'cases': [{'code': '>>> \n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'q2b',
+    'points': 2,
+    'suites': [   {   'cases': [{'code': ">>> pd.read_csv('data/t2_q2_df.csv', index_col=None).equals(catch_data)\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
