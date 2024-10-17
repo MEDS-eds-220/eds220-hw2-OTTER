c14caed995e30d1cd13bf62cb01f04bcf64fe90b
annieradams <adams.annie11@gmail.com>
2024-10-17 15:56:32 -0700

    trying making q4 manual s=to see if autograde will work

--- dist-task3/autograder/tests/q7.py
+++ dist-task3/autograder/tests/q7.py
@@ -2,7 +2,7 @@ OK_FORMAT = True
 
 test = {   'name': 'q7',
     'points': 2,
-    'suites': [   {   'cases': [{'code': ">>> pd.read_csv('data/t3_q7_df.csv', index_col='date', parse_dates=True).equals(aqi_sb)\nTrue", 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': ">>> assert pd.read_csv('data/t3_q7_df.csv', index_col='date', parse_dates=True).equals(aqi_sb)\n", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
--- dist-task3/student/tests/q6a.py
+++ dist-task3/student/tests/q6a.py
@@ -2,7 +2,7 @@ OK_FORMAT = True
 
 test = {   'name': 'q6a',
     'points': 2,
-    'suites': [   {   'cases': [{'code': ">>> assert pd.read_csv('data/t3_q6a_df.csv', index_col=0).equals(aqi_sb)\n", 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': ">>> pd.read_csv('data/t3_q6a_df.csv').equals(aqi_sb.reset_index(drop=True))\nTrue", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
