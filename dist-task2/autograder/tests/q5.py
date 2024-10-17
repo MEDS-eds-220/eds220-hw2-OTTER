e24b3974336575769292c2bb6662952400acecae
annieradams <adams.annie11@gmail.com>
2024-10-16 17:27:01 -0700

    task 2 reay for final updates, autograder running, task 3 autograder not running

--- dist-task2/autograder/tests/q5.py
+++ dist-task2/autograder/tests/q5.py
@@ -1,3 +1,12 @@
 OK_FORMAT = True
 
-test = {'name': 'q5', 'points': 2, 'suites': [{'cases': [{'code': '>>> \n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'q5',
+    'points': 2,
+    'suites': [   {   'cases': [   {'code': '>>> len(catch_I) == 1\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> bool(catch_I.iloc[0, 1] == 1955)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': ">>> bool(catch_I.iloc[0, 0] == 'GSE')\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> bool(catch_I.iloc[0, 3] == 'I')\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
--- dist-task3/autograder/tests/q9.py
+++ dist-task3/autograder/tests/q9.py
@@ -2,11 +2,7 @@ OK_FORMAT = True
 
 test = {   'name': 'q9',
     'points': 2,
-    'suites': [   {   'cases': [   {   'code': ">>> solution = pd.read_csv('data/t3_q9_df.csv', index_col='date')\n"
-                                               '>>> solution.index = pd.to_datetime(solution.index)\n'
-                                               '>>> assert aqi_sb.equals(solution)\n',
-                                       'hidden': False,
-                                       'locked': False}],
+    'suites': [   {   'cases': [{'code': ">>> assert pd.read_csv('data/t3_q9_df.csv', index_col='date', parse_dates=True).equals(aqi_sb)\n", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
