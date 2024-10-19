cbdff392c5a37322df1697b5a44fb604f9eae5d7
annieradams <adams.annie11@gmail.com>
2024-10-18 17:10:49 -0700

    adding most recent autograder zip file

--- dist-task3/student/tests/q6a.py
+++
@@ -1,9 +0,0 @@
-OK_FORMAT = True
-
-test = {   'name': 'q6a',
-    'points': 2,
-    'suites': [   {   'cases': [{'code': ">>> pd.testing.assert_frame_equal(pd.read_csv('data/t3_q6a_df.csv'), aqi_sb.reset_index(drop=True))\n", 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
--- dist-task3/student/tests/q7.py
+++
@@ -1,9 +0,0 @@
-OK_FORMAT = True
-
-test = {   'name': 'q7',
-    'points': 2,
-    'suites': [   {   'cases': [{'code': ">>> pd.testing.assert_frame_equal(pd.read_csv('data/t3_q7_df.csv', index_col='date', parse_dates=True), aqi_sb)\n", 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
