d1ae9eca4523f88fac581e0704f02c11c1a0505c
annieradams <adams.annie11@gmail.com>
2024-10-21 12:07:57 -0700

    update all otter files for task 3, gradescope questions all running

--- dist-task3/autograder/tests/q7.py
+++ dist-task3/autograder/tests/q7.py
@@ -5,9 +5,12 @@ test = {   'name': 'q7',
     'suites': [   {   'cases': [   {   'code': '>>> def test_q7(aqi_sb):\n'
                                                '...     try:\n'
                                                "...         expected_data = pd.read_csv('data/t3_q7_df.csv')\n"
-                                               '...         pd.testing.assert_frame_equal(expected_data, aqi_sb.reset_index(drop=True))\n'
+                                               '...         expected_data.date = pd.to_datetime(expected_data.date)\n'
+                                               "...         expected_data = expected_data.set_index('date')\n"
+                                               '...         pd.testing.assert_frame_equal(expected_data, aqi_sb)\n'
                                                '...     except AssertionError:\n'
-                                               "...         raise AssertionError('Incorrect answer.')\n",
+                                               "...         raise AssertionError('Incorrect answer.')\n"
+                                               '>>> test_q7(aqi_sb)\n',
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
