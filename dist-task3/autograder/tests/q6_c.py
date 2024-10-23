a8a6d7d1fa49ed363daeb4366a915c2c670f258d
annieradams <adams.annie11@gmail.com>
2024-10-23 10:17:59 -0700

    clean up repo

---
+++ dist-task3/autograder/tests/q6_c.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q6_c',
+    'points': 2,
+    'suites': [{'cases': [{'code': ">>> date_type == pd.api.types.pandas_dtype('O')\nTrue", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
