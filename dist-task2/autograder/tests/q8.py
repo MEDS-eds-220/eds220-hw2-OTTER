5697c2ef802ed731d72df75d25c0d868c80ecc8b
annieradams <adams.annie11@gmail.com>
2024-10-25 11:46:45 -0700

    make question 8 on task 2 a manual question

--- dist-task2/autograder/tests/q8.py
+++
@@ -1,9 +0,0 @@
-OK_FORMAT = True
-
-test = {   'name': 'q8',
-    'points': 2,
-    'suites': [   {   'cases': [{'code': ">>> pd.read_csv('data/t2_q8_df.csv', index_col='Region').round(4).equals(pd.DataFrame(avg_region.round(4)))\nTrue", 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
