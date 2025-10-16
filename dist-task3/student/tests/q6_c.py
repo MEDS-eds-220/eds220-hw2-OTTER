e8c771b78092185b469150ebcd99b02c1e8ea514
annieradams <adams.annie11@gmail.com>
2025-10-16 20:34:08 +0000

    Make suggested changes to 2025 notebooks, update task 3 7c test

--- dist-task3/student/tests/q6_c.py
+++
@@ -1,5 +0,0 @@
-OK_FORMAT = True
-
-test = {   'name': 'q6_c',
-    'points': 2,
-    'suites': [{'cases': [{'code': ">>> date_type == pd.api.types.pandas_dtype('O')\nTrue", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
